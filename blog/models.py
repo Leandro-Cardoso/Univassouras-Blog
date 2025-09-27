ad38a2edb1e193cec3ac49dd17d5f2a67e519a76
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-09-27 13:06:51 -0300

    Model Role and User created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    COMMON_ROLE_ID = 1
+
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    role = models.ForeignKey(
+        'Role',
+        on_delete = models.SET_NULL,
+        null = True,
+        blank = True,
+        default = COMMON_ROLE_ID
+    )
+
+    def __str__(self):
+        return self.username
