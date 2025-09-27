079f36ec3d3ba92545f736dafd59baaaff4d0124
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-09-27 14:14:40 -0300

    Create initial roles and user admin

--- blog/models.py
+++ blog/models.py
@@ -3,25 +3,25 @@ from django.db import models
 # Create your models here.
 
 class Role(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length = 100, unique = True, null = False)
+    description = models.TextField(blank = True)
 
     def __str__(self):
         return self.name
 
 class User(models.Model):
-    COMMON_ROLE_ID = 1
+    DEFAULT_ROLE_ID = 1
 
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    username = models.CharField(max_length = 100, unique = True, null = False)
+    password = models.CharField(max_length = 100, null = False)
+    created_at = models.DateTimeField(auto_now_add = True)
 
     role = models.ForeignKey(
         'Role',
         on_delete = models.SET_NULL,
         null = True,
         blank = True,
-        default = COMMON_ROLE_ID
+        default = DEFAULT_ROLE_ID
     )
 
     def __str__(self):
