5fb4fb9d99fcfca118714b3a43e2fd7b91d27295
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-10-02 14:58:57 -0300

    Comment model

--- blog/models.py
+++ blog/models.py
@@ -53,3 +53,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    author = models.CharField(max_length = 100, null = False)
+    created_at = models.DateTimeField(auto_now_add = True)
+    content = models.TextField(null = False)
+
+    post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+
+    def __str__(self):
+        return f"{self.author} - {self.post.title}"
