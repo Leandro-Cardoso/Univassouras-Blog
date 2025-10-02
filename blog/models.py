664954f932a2e8c51f1c2d1ff6f96c17e92a6274
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-10-02 14:47:49 -0300

    Post model

--- blog/models.py
+++ blog/models.py
@@ -33,4 +33,23 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Post(models.Model):
+    title = models.CharField(max_length = 100, unique = True, null = False)
+    created_at = models.DateTimeField(auto_now_add = True)
+    content = models.TextField(null = False)
 
+    author = models.ForeignKey(
+        'User',
+        on_delete = models.SET_NULL,
+        null = True,
+        blank = True
+    )
+    category = models.ForeignKey(
+        'Category',
+        on_delete = models.SET_NULL,
+        null = True,
+        blank = True
+    )
+
+    def __str__(self):
+        return self.title
