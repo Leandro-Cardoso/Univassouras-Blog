20b684f683df3525b78819efe2689843a70ea3f2
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-10-04 12:50:27 -0300

    List posts implementation

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "blog"
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = [
+        "title",
+        "category",
+        "content"
+    ]
+    success_url = reverse_lazy("index")
