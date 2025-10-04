bc0317528afa977f18482fc16003fe379467519d
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-10-04 18:39:10 -0300

    Comments system

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostCreateView, CategoryCreateView
+from .views import PostListView, PostCreateView, CategoryCreateView, create_comment
 
 urlpatterns = [
     path("", PostListView.as_view(), name="index"),
     path("create", PostCreateView.as_view(), name = "create_post"),
-    path("category", CategoryCreateView.as_view(), name = "create_category")
+    path("category", CategoryCreateView.as_view(), name = "create_category"),
+    path('post/<int:post_pk>/comment/new/', create_comment, name='create_comment'), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView
+from django.views.decorators.http import require_POST
 from django.urls import reverse_lazy
 from .models import Post, Category
+from .forms import CommentForm
 
 class PostListView(ListView):
     model = Post
     template_name = "blog/index.html"
     context_object_name = "blog"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm() 
+        return context
 
 class PostCreateView(CreateView):
     model = Post
@@ -26,3 +29,18 @@ class CategoryCreateView(CreateView):
         "name"
     ]
     success_url = reverse_lazy("create_post")
+
+@require_POST
+def create_comment(request, post_pk):
+    post = Post.objects.get(pk = post_pk)
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        new_comment = form.save(commit = False) 
+        new_comment.post = post 
+        new_comment.save()
+        return redirect('index') 
+    context = {
+        'blog': Post.objects.all(),
+        'comment_form': form
+    }
+    return render(request, 'blog/index.html', context)
